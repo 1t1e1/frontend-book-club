316732b0f6b5c170beb7daa374a0674c526812c6
1t1e1 <1t1ebir@gmail.com>
2022-03-07 01:38:44 +0300

    found  out mdx render component.

--- gatsby/src/pages/book/{mdx.slug}.js
+++ gatsby/src/pages/book/{mdx.slug}.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import { MDXRenderer } from "gatsby-plugin-mdx";
+
 export const query = graphql`
 	query ($slug: String = "") {
 		mdx(slug: { eq: $slug }) {
-			id
+			body
+			mdxAST
 			frontmatter {
 				author
 				description
@@ -13,14 +16,18 @@ export const query = graphql`
 		}
 	}
 `;
+
 export default function BookDetail({ data }) {
-	console.log("data from book id", data);
+	console.log("data from book id", data.mdx.mdxAST);
 	return (
 		<div>
 			Book detail page
 			<h1>Title: {data.mdx.frontmatter.title}</h1>
 			<p>Description: {data.mdx.frontmatter.description}</p>
-			{/* <h5>author: {data.mdx.frontmatter.author}</h5> */}
+			<h5>author: {data.mdx.frontmatter.author}</h5>
+			<div>
+				<MDXRenderer>{data.mdx.body}</MDXRenderer>
+			</div>
 		</div>
 	);
 }

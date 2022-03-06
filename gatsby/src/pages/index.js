ea8a10a0e1d34f1b72816ae34003fced8287716d
1t1e1 <1t1ebir@gmail.com>
2022-03-06 03:49:22 +0300

    add basic markdown content properly

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,24 +1,26 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../components/ProTip';
-import Link from '../components/Link';
-import Copyright from '../components/Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ProTip from "../components/ProTip";
+import Link from "../components/Link";
+import Copyright from "../components/Copyright";
 
 export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Gatsby example
-        </Typography>
-        <Link to="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+	return (
+		<Container maxWidth="sm">
+			<Box sx={{ my: 4, border: 1 }}>
+				<Box sx={{ my: 4, border: 1 }}>
+					<Typography variant="h4" component="h1" gutterBottom>
+						Gatsby example
+					</Typography>
+					<Link to="/about" color="secondary">
+						Go to the about page
+					</Link>
+					<ProTip />
+					<Copyright />
+				</Box>
+			</Box>
+		</Container>
+	);
 }

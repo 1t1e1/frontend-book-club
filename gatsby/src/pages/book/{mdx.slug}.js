60713273f955895b36d58a0f19c301d6d2ae7387
1t1e1 <1t1ebir@gmail.com>
2022-03-08 22:26:01 +0300

    add dynamic image

--- gatsby/src/pages/book/{mdx.slug}.js
+++ gatsby/src/pages/book/{mdx.slug}.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
@@ -12,19 +13,41 @@ export const query = graphql`
 				author
 				description
 				title
+				cover {
+					absolutePath
+					childImageSharp {
+						fluid {
+							base64
+							tracedSVG
+							srcWebp
+							srcSetWebp
+							originalImg
+							originalName
+						}
+						gatsbyImageData
+					}
+				}
 			}
 		}
 	}
 `;
 
+// img fix  fluid ..
 export default function BookDetail({ data }) {
-	console.log("data from book id", data.mdx.mdxAST);
+	console.log(
+		"data from book id",
+
+		data.mdx.frontmatter.cover.childImageSharp.gatsbyImageData
+	);
 	return (
 		<div>
 			Book detail page
 			<h1>Title: {data.mdx.frontmatter.title}</h1>
 			<p>Description: {data.mdx.frontmatter.description}</p>
 			<h5>author: {data.mdx.frontmatter.author}</h5>
+			<GatsbyImage
+				image={data.mdx.frontmatter.cover.childImageSharp.gatsbyImageData}
+			/>
 			<div>
 				<MDXRenderer>{data.mdx.body}</MDXRenderer>
 			</div>
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { graphql } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
@@ -26,13 +26,18 @@ export default function Index({ data }) {
 						Book List
 					</Typography>
 					<List>
-						{data.allMdx.edges.map(({ node }, index) => (
-							<ListItem key={index}>
-								<Link to={`/book/${node.slug}`} color="secondary">
-									{node.frontmatter.title}
-								</Link>
-							</ListItem>
-						))}
+						{data.allMdx.edges.map(({ node }, index) => {
+							const image = getImage(node.frontmatter.cover?.childImageSharp);
+
+							return (
+								<ListItem key={index}>
+									<Link to={`/book/${node.slug}`} color="secondary">
+										{node.frontmatter.title}
+									</Link>
+									<GatsbyImage image={image} maxWidth="100px" />
+								</ListItem>
+							);
+						})}
 					</List>
 					<ProTip />
 					<Copyright />
@@ -52,6 +57,11 @@ export const query = graphql`
 					frontmatter {
 						title
 						author
+						cover {
+							childImageSharp {
+								gatsbyImageData(height: 100)
+							}
+						}
 					}
 				}
 			}

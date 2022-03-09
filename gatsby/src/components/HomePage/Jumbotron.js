4d7b7c107e81e52ebc7dd2b3c1f5ae98f82f0600
1t1e1 <1t1ebir@gmail.com>
2022-03-09 18:19:30 +0300

    changing Jumbotron a little

--- gatsby/src/components/HomePage/Jumbotron.js
+++ gatsby/src/components/HomePage/Jumbotron.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 import { StaticImage } from "gatsby-plugin-image";
+import { Box } from "@mui/material";
 
 export default function Jumbotron() {
 	return (
-		<StaticImage
-			alt="Home page cover image"
-			src="../statics/homepageCover.jpg"
-		/>
+		<>
+			<StaticImage
+				alt="Home page cover image"
+				src="../statics/homepageCover.jpg"
+				style={{
+					height: "14em",
+					position: "absolute",
+				}}
+				imgStyle={{
+					objectFit: "cover",
+					clipPath: "polygon(0% 0%, 100% 0, 100%  calc(100% - 30px), 0% 100%)",
+				}}
+			/>
+			<Box
+				sx={{
+					clipPath: "polygon(0% 0%, 100% 0, 100%  calc(100% - 30px), 0% 100%)",
+					zIndex: 9,
+					width: "100%",
+					height: "14em",
+					position: "relative",
+					backgroundColor: "black",
+					opacity: 0.7,
+					mb: 4,
+				}}
+			></Box>
+		</>
 	);
 }

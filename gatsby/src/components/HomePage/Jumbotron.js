5ed6d580d748b03a726bfdee1ad3c0d66eb3de39
1t1e1 <1t1ebir@gmail.com>
2022-03-09 22:55:51 +0300

    edit cover image component

--- gatsby/src/components/HomePage/Jumbotron.js
+++ gatsby/src/components/HomePage/Jumbotron.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 import { StaticImage } from "gatsby-plugin-image";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
 
 export default function Jumbotron() {
 	return (
-		<>
+		<Box>
 			<StaticImage
 				alt="Home page cover image"
 				src="../statics/homepageCover.jpg"
 				style={{
-					height: "14em",
+					height: "24em",
 					position: "absolute",
 				}}
 				imgStyle={{
-					objectFit: "cover",
 					clipPath: "polygon(0% 0%, 100% 0, 100%  calc(100% - 30px), 0% 100%)",
+					// better clipPath: "polygon(0% 0%, 100% 0, 100%  calc(100% - 9vw), 0% 100%)",
 				}}
 			/>
 			<Box
@@ -23,13 +24,50 @@ export default function Jumbotron() {
 					clipPath: "polygon(0% 0%, 100% 0, 100%  calc(100% - 30px), 0% 100%)",
 					zIndex: 9,
 					width: "100%",
-					height: "14em",
-					position: "relative",
+					height: "24em",
 					backgroundColor: "black",
 					opacity: 0.7,
+					position: "absolute",
+				}}
+			/>
+			<Container
+				maxWidth="lg"
+				sx={{
+					zIndex: 19,
+					position: "relative",
+					height: "24em",
 					mb: 4,
 				}}
-			></Box>
-		</>
+			>
+				<Typography
+					variant="h2"
+					component="h1"
+					sx={{
+						WebkitTextStrokeWidth: "1px",
+						WebkitTextStrokeColor: "#fff",
+					}}
+				>
+					Join
+				</Typography>
+				<Typography
+					variant="h2"
+					component="h1"
+					sx={{
+						color: "#fff",
+					}}
+				>
+					FrontEnd
+				</Typography>
+				<Typography
+					variant="h2"
+					component="h1"
+					sx={{
+						color: "#fff",
+					}}
+				>
+					Book Club
+				</Typography>
+			</Container>
+		</Box>
 	);
 }

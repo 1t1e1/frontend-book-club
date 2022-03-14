49354afa907736dc4d10715bb3420c135f134b5a
1t1e1 <1t1ebir@gmail.com>
2022-03-14 10:07:52 +0300

    add accordion to book id page,

---
+++ src/components/Book/MdxComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Accordion from "@mui/material/Accordion";
+
+function MyAccordion({ title, children }) {
+	return (
+		<Accordion>
+			<AccordionSummary>{title}</AccordionSummary>
+			<AccordionDetails>{children}</AccordionDetails>
+		</Accordion>
+	);
+}
+
+const components = {
+	MyAccordion: MyAccordion,
+};
+
+export default components;
--- src/pages/book/{mdx.slug}.js
+++ src/pages/book/{mdx.slug}.js
@@ -1,8 +1,31 @@
 import React from "react";
+
 import { graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { MDXProvider } from "@mdx-js/react";
+
+import components from "../../components/Book/MdxComponent";
+
+export default function BookDetail({ data }) {
+	return (
+		<div>
+			Book detail page
+			<h1>Title: {data.mdx.frontmatter.title}</h1>
+			<p>Description: {data.mdx.frontmatter.description}</p>
+			<h5>author: {data.mdx.frontmatter.author}</h5>
+			<GatsbyImage
+				image={data.mdx.frontmatter.cover.childImageSharp.gatsbyImageData}
+			/>
+			<div>
+				<MDXProvider components={components}>
+					<MDXRenderer>{data.mdx.body}</MDXRenderer>
+				</MDXProvider>
+			</div>
+		</div>
+	);
+}
 
 export const query = graphql`
 	query ($slug: String) {
@@ -23,20 +46,3 @@ export const query = graphql`
 		}
 	}
 `;
-
-export default function BookDetail({ data }) {
-	return (
-		<div>
-			Book detail page
-			<h1>Title: {data.mdx.frontmatter.title}</h1>
-			<p>Description: {data.mdx.frontmatter.description}</p>
-			<h5>author: {data.mdx.frontmatter.author}</h5>
-			<GatsbyImage
-				image={data.mdx.frontmatter.cover.childImageSharp.gatsbyImageData}
-			/>
-			<div>
-				<MDXRenderer>{data.mdx.body}</MDXRenderer>
-			</div>
-		</div>
-	);
-}

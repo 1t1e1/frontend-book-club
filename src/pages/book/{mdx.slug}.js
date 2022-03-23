72077b7d276f9161b2e3c5d9e02e6f899511c648
1t1e1 <1t1ebir@gmail.com>
2022-03-23 19:52:14 +0300

    change layout of book detail page

--- src/pages/book/{mdx.slug}.js
+++ src/pages/book/{mdx.slug}.js
@@ -1,29 +1,45 @@
 import React from "react";
 
 import { graphql } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { MDXProvider } from "@mdx-js/react";
 
+import { Grid, Box, Card, Typography } from "@mui/material";
+
 import components from "../../components/Book/MdxComponent";
 
 export default function BookDetail({ data }) {
+	const { author, description, title, subtitle, cover } = data.mdx.frontmatter;
+	const image = getImage(cover.childImageSharp.gatsbyImageData);
+
 	return (
-		<div>
-			Book detail page
-			<h1>Title: {data.mdx.frontmatter.title}</h1>
-			<p>Description: {data.mdx.frontmatter.description}</p>
-			<h5>author: {data.mdx.frontmatter.author}</h5>
-			<GatsbyImage
-				image={data.mdx.frontmatter.cover.childImageSharp.gatsbyImageData}
-			/>
-			<div>
+		<Box
+			sx={{
+				px: 5,
+				py: 10,
+			}}
+		>
+			<Grid container spacing={3} justifyContent="space-evenly">
+				<Grid item sm={3} md={3}>
+					<Box sx={{ pt: 5 }}>
+						<GatsbyImage alt={`image of `} image={image} />
+					</Box>
+				</Grid>
+				<Grid item sm={9} md={8}>
+					<h1>{title}</h1>
+					<h2>{subtitle}</h2>
+					<h5>{author}</h5>
+					<p>{description}</p>
+				</Grid>
+			</Grid>
+			<Grid item sm={12}>
 				<MDXProvider components={components}>
 					<MDXRenderer>{data.mdx.body}</MDXRenderer>
 				</MDXProvider>
-			</div>
-		</div>
+			</Grid>
+		</Box>
 	);
 }
 
@@ -36,6 +52,7 @@ export const query = graphql`
 				author
 				description
 				title
+				subtitle
 				cover {
 					absolutePath
 					childImageSharp {

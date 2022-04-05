5fc4346b89da888961661201324c6d958d2151af
1t1e1 <1t1ebir@gmail.com>
2022-04-05 08:56:57 +0300

    add react visiblity sensor

--- src/components/HomePage/BookCard.js
+++ src/components/HomePage/BookCard.js
@@ -3,131 +3,148 @@ import { GatsbyImage } from "gatsby-plugin-image";
 
 import { Grid, Box, Card, Typography } from "@mui/material";
 
+import ReactVisibilitySensor from "react-visibility-sensor";
+
 import Link from "../Link";
 
 export default function BookCard({ book, image, index }) {
 	const { author, description, title, subtitle } = book.frontmatter;
 
+	function handleChangeVisiblity(isVisible) {
+		console.log(index, " index vis ", isVisible ? "true" : "false");
+	}
+
 	return (
 		<Grid item xs={12}>
-			<Card
-				elevation={0}
-				sx={{
-					backgroundColor: "#abdecd",
-					zIndex: 3,
-					position: "relative",
-					overflow: "visible",
-					pb: 4,
-					borderRadius: index % 2 ? "0px 10px 10px 0" : "10px 0px 0px 10px",
-					"&::before": {
-						position: "absolute",
-						zIndex: -1,
-						// backgroundColor: "#eda",
-						backgroundColor: "#abdecd",
-						top: "-28px",
-						content: "''",
-						height: "30px",
-						width: "100%",
-						clipPath:
-							index % 2
-								? "polygon(0 100%, 100% 100%, 0 0)"
-								: "polygon(0 100%, 100% 0, 100% 100%)",
-					},
-					"&::after": {
-						position: "absolute",
-						zIndex: -1,
-						backgroundColor: "#abdecd",
-						bottom: "-28px",
-						content: "''",
-						height: "30px",
-						width: "100%",
-						clipPath:
-							index % 2
-								? "polygon(0 100%, 100% 0, 0 0)"
-								: "polygon(0 0, 100% 0, 100% 100%)",
-					},
+			<ReactVisibilitySensor
+				onChange={handleChangeVisiblity}
+				offset={{
+					top: 15,
+					bottom: 15,
 				}}
+				partialVisibility
 			>
-				<Grid
-					container
-					spacing={1}
-					sx={{
-						justifyContent: "space-evenly",
-						flexDirection: index % 2 ? "row-reverse" : "row",
-					}}
-				>
-					<Grid
-						item
-						xs={8}
-						md={3}
+				{({ isVisible }) => (
+					<Card
+						elevation={0}
 						sx={{
-							height: "300px",
+							backgroundColor: "#abdecd",
+							zIndex: 3,
 							position: "relative",
-							"&>div": {
-								zIndex: "9",
-								top: "-60px",
+							overflow: "visible",
+							pb: 4,
+							borderRadius: index % 2 ? "0px 10px 10px 0" : "10px 0px 0px 10px",
+							"&::before": {
 								position: "absolute",
-								boxShadow: 13,
+								zIndex: -1,
+								// backgroundColor: "#eda",
+								backgroundColor: "#abdecd",
+								top: "-28px",
+								content: "''",
+								height: "30px",
+								width: "100%",
+								clipPath:
+									index % 2
+										? "polygon(0 100%, 100% 100%, 0 0)"
+										: "polygon(0 100%, 100% 0, 100% 100%)",
+							},
+							"&::after": {
+								position: "absolute",
+								zIndex: -1,
+								backgroundColor: "#abdecd",
+								bottom: "-28px",
+								content: "''",
+								height: "30px",
+								width: "100%",
+								clipPath:
+									index % 2
+										? "polygon(0 100%, 100% 0, 0 0)"
+										: "polygon(0 0, 100% 0, 100% 100%)",
 							},
 						}}
 					>
-						<GatsbyImage alt={`image of ${title}`} image={image} />
-					</Grid>
-
-					<Grid item xs={11} md={6}>
-						<Box
+						<Grid
+							container
+							spacing={1}
 							sx={{
-								display: "flex",
-								flexDirection: "column",
 								justifyContent: "space-evenly",
-								height: "max(100%,220px)",
+								flexDirection: index % 2 ? "row-reverse" : "row",
 							}}
 						>
-							<Typography
-								variant="h3"
+							<Grid
+								item
+								xs={8}
+								md={3}
 								sx={{
-									textAlign: "center",
-									m: 2,
+									height: "300px",
+									position: "relative",
+									"&>div": {
+										zIndex: "9",
+										top: "-60px",
+										position: "absolute",
+										boxShadow: 13,
+									},
 								}}
 							>
-								<Link to={`/book/${book.slug}`} color="inherit">
-									{title}
-								</Link>
-							</Typography>
+								<GatsbyImage alt={`image of ${title}`} image={image} />
+							</Grid>
 
-							<Typography
-								variant="h5"
-								sx={{
-									textAlign: "center",
-								}}
-							>
-								{subtitle}
-							</Typography>
+							<Grid item xs={11} md={6}>
+								<Box
+									sx={{
+										display: "flex",
+										flexDirection: "column",
+										justifyContent: "space-evenly",
+										height: "max(100%,220px)",
+									}}
+								>
+									<Typography
+										variant="h3"
+										sx={{
+											textAlign: "center",
+											m: 2,
+										}}
+									>
+										<Link to={`/book/${book.slug}`} color="inherit">
+											{title}
+										</Link>
+									</Typography>
 
-							<Typography
-								variant="h4"
-								sx={{
-									textAlign: "center",
-									m: 2,
-								}}
-							>
-								{author}
-							</Typography>
-							<Typography
-								variant="body1"
-								sx={{
-									mx: "auto",
-									mb: 5,
-									textAlign: "center",
-									maxWidth: "80%",
-								}}
-							>
-								{description}
-							</Typography>
-						</Box>
-					</Grid>
-				</Grid>
-			</Card>
+									<Typography
+										variant="h5"
+										sx={{
+											textAlign: "center",
+										}}
+									>
+										{subtitle}
+									</Typography>
+
+									<Typography
+										variant="h4"
+										sx={{
+											textAlign: "center",
+											m: 2,
+										}}
+									>
+										{author}
+									</Typography>
+									<Typography
+										variant="body1"
+										sx={{
+											mx: "auto",
+											mb: 5,
+											textAlign: "center",
+											maxWidth: "80%",
+										}}
+									>
+										{description}
+									</Typography>
+								</Box>
+							</Grid>
+						</Grid>
+					</Card>
+				)}
+			</ReactVisibilitySensor>
 		</Grid>
 	);
 }
